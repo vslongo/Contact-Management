8935b5f5514e3a835b96af41d6c64f342e5622c3
Vinicius Slongo <66139105+vslongo@users.noreply.github.com>
2025-06-27 11:52:41 -0300

    Update Contact.php

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -1,11 +1,12 @@
-<?php
+   namespace App\Models;
 
-namespace App\Models;
+   use Illuminate\Database\Eloquent\Factories\HasFactory;
+   use Illuminate\Database\Eloquent\Model;
+   use Illuminate\Database\Eloquent\SoftDeletes;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+   class Contact extends Model
+   {
+       use HasFactory, SoftDeletes;
 
-class Contact extends Model
-{
-    use HasFactory;
-}
+       protected $fillable = ['name', 'contact', 'email'];
+   }
