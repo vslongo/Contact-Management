aedc416b126d695076873d7323a99d5a97776251
Vinicius Slongo <66139105+vslongo@users.noreply.github.com>
2025-06-27 12:11:33 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,10 @@ Route::get('/', function () {
     return redirect()->route('contacts.index');
 });
 
-Route::resource('contacts', ContactController::class);
+// Rotas de autenticação
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Auth::routes();
+// Rotas do recurso de contatos
+Route::resource('contacts', ContactController::class);
